08d6336a85fa9a4c23d0de70065f8e4f656bdea9
Daichi Iida <daichi.iida@nextbeat.net>
2024-06-05 06:51:43 +0000

    add update toast

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { page, navigating } from '$app/stores';
+	import { page, navigating, updated } from '$app/stores';
 </script>
 
 <nav>
@@ -14,11 +14,48 @@
 
 <slot />
 
+<button
+	type="button"
+	on:click={() => {
+		updated.check();
+	}}>Check update</button
+>
+
+{#if $updated}
+	<div class="toast">
+		<p>
+			A new version of the app is available
+
+			<button on:click={() => location.reload()}> reload the page </button>
+		</p>
+	</div>
+{/if}
+
 <style>
 	a {
 		text-decoration: none;
 		&[aria-current='true'] {
 			border-bottom: 2px solid;
 		}
+	}
+
+	.toast {
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		padding: 1rem;
+		display: flex;
+		justify-content: center;
+		gap: 1rem;
+	}
+
+	.toast p {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		background: var(--bg-2);
+		padding: 0.5rem 0.5rem 0.5rem 1rem;
+		border-radius: 4px;
 	}
 </style>

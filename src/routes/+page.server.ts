f50bc95e189268f6f669592e1c077d6cb5590618
Daichi Iida <daichi.iida@nextbeat.net>
2024-06-05 02:48:28 +0000

    add cookies

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,10 @@
 import type { PageServerLoadEvent } from './$types';
 
-export function load({ setHeaders }: PageServerLoadEvent) {
-	setHeaders({ 'Content-Type': 'text/plain' });
+export function load({ cookies }: PageServerLoadEvent) {
+	const visitedCookieKey = 'visited';
+	const visited = cookies.get(visitedCookieKey) === 'true';
+
+	cookies.set(visitedCookieKey, 'true', { path: '/' });
+
+	return { visited };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>home</h1>
+<script>
+	export let data;
+</script>
+
+<h1>Hello {data.visited ? 'friend' : 'stranger'}!</h1>
 <p>this is the home page.</p>

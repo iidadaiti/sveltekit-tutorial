c80725f48226666f861463770324c3f2ec2d7af5
Daichi Iida <daichi.iida@nextbeat.net>
2024-06-05 09:57:40 +0000

    add trailing slash options

---
+++ part4/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<nav>
+	<a href="/trailing-slash/always">/trailing-slash/always</a>
+	<a href="/trailing-slash/always/">/trailing-slash/always/</a>
+	<a href="/trailing-slash/ignore">/trailing-slash/ignore</a>
+	<a href="/trailing-slash/ignore/">/trailing-slash/ignore/</a>
+	<a href="/trailing-slash/never">/trailing-slash/never</a>
+	<a href="/trailing-slash/never/">/trailing-slash/never/</a>
+</nav>
+
+<slot />

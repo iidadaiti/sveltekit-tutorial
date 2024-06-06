138fe080509e1e315658acd5e883c0f24c232bed
Daichi Iida <daichi.iida@nextbeat.net>
2024-06-06 06:01:30 +0000

    add dynamic env

--- part4/src/routes/env/static/private/+page.server.ts
+++ part4/src/routes/env/static/private/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect, fail } from '@sveltejs/kit';
-import { PASSPHRASE } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 export function load({ cookies }) {
 	if (cookies.get('allowed')) {
@@ -11,7 +11,7 @@ export const actions = {
 	default: async ({ request, cookies }) => {
 		const data = await request.formData();
 
-		if (data.get('passphrase') === PASSPHRASE) {
+		if (data.get('passphrase') === env.PASSPHRASE) {
 			cookies.set('allowed', 'true', {
 				path: '/env/static/private'
 			});

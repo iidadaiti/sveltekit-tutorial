be80d292b6e52393d468b3cfff2108449f2c0943
Daichi Iida <daichi.iida@nextbeat.net>
2024-06-06 03:07:24 +0000

    add await parent

---
+++ part4/src/routes/await-parent/+page.svelte
@@ -0,0 +1,2 @@
+<p>if a = 1 and b = a + 1, what is a + b?</p>
+<a href="/await-parent/sum">show answer</a>
---
+++ part4/src/routes/await-parent/sum/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	export let data;
+</script>
+
+<p>{data.a} + {data.b} = {data.c}</p>
+<p><a href="/await-parent">home</a></p>

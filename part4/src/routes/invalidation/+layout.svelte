a54bff643251d6c05cd1a4d14c11982a0d23b6e0
Daichi Iida <daichi.iida@nextbeat.net>
2024-06-06 03:17:53 +0000

    add invalidation

---
+++ part4/src/routes/invalidation/+layout.svelte
@@ -0,0 +1,13 @@
+<nav>
+	<a href="/invalidation/America/New_York">New York</a>
+	<a href="/invalidation/America/Sao_Paulo">São Paulo</a>
+	<a href="/invalidation/Europe/London">London</a>
+	<a href="/invalidation/Europe/Paris">Paris</a>
+	<a href="/invalidation/Africa/Cairo">Cairo</a>
+	<a href="/invalidation/Asia/Shanghai">Shanghai</a>
+	<a href="/invalidation/Asia/Tokyo">Tokyo</a>
+	<a href="/invalidation/Australia/Sydney">Sydney</a>
+	<a href="/invalidation/America/Los_Angeles">Los Angeles</a>
+</nav>
+
+<slot />

696d18a6369ecccb134f8b400af709d29c4c209a
Daichi Iida <daichi.iida@nextbeat.net>
2024-06-06 06:03:39 +0000

    add dynamic public  env

--- part4/src/routes/env/static/private/+page.svelte
+++ part4/src/routes/env/static/private/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-	import { PUBLIC_THEME_BACKGROUND, PUBLIC_THEME_FOREGROUND } from '$env/static/public';
+	import { env } from '$env/dynamic/public';
 
 	export let form;
 </script>
 
-<main style:background={PUBLIC_THEME_BACKGROUND} style:color={PUBLIC_THEME_FOREGROUND}>
-	{PUBLIC_THEME_FOREGROUND} on {PUBLIC_THEME_BACKGROUND}
+<main style:background={env.PUBLIC_THEME_BACKGROUND} style:color={env.PUBLIC_THEME_FOREGROUND}>
+	{env.PUBLIC_THEME_FOREGROUND} on {env.PUBLIC_THEME_BACKGROUND}
 </main>
 
 <form method="POST">
